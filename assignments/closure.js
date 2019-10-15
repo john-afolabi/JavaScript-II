0cffc99eba98af580fafc8cc847bfc217849b182
John Afolabi <johnafolabi.da@gmail.com>
2019-10-15 15:54:28 +0100

    Complete closure.js assignments

--- assignments/closure.js
+++ assignments/closure.js
@@ -4,6 +4,14 @@
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
 
+const parent = () => {
+  let number = 1;
+  const child = () => {
+    console.log(`I am ${number} level deep`);
+  };
+  child();
+};
+parent();
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
